84ae372c813d67c7cf1ac0edbfd6002cb43dfbd7
Nuno Ramos <nmiguel123@gmail.com>
2025-05-27 09:56:18 +0100

    volume mixer

---
+++ home/.config/system_scripts/volume_mixer.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python3
+import os
+import sys
+import json
+import subprocess
+
+STEP = "5%"
+
+def build_menu():
+    # Call pactl and parse JSON
+    proc = subprocess.run(
+        ["pactl", "--format", "json", "list", "sink-inputs"],
+        check=True,
+        stdout=subprocess.PIPE,
+        text=True
+    )
+    sinks = json.loads(proc.stdout)
+    entries = []
+    for sink in sinks:
+        idx = sink["index"]
+        name = sink.get("properties", {}).get("application.name", "unknown")
+        vol = sink["volume"]["front-left"]["value_percent"]
+        entries.append({"id": idx, "name": name, "volume": vol})
+    return entries
+
+def print_menu(entries):
+    for i, entry in enumerate(entries):
+        text = f"{entry['name'].title()}: {entry['volume']}"
+
+        print(text)
+
+def get_selected_id(entries) -> int:
+    if len(sys.argv) != 2:
+        return -1
+
+    sel = sys.argv[1].split(":")[0].lower()
+    for i, entry in enumerate(entries):
+        if sel == entry["name"].lower():
+            return i
+
+    return -1
+
+
+def main():
+
+    # Set rofi options
+    print("\0use-hot-keys\x1ftrue")
+    print("\0keep-selection\x1ftrue")
+    print("\0keep-filter\x1ftrue")
+
+    entries = build_menu()
+
+    # Get Rofi return code and selection index
+    ret = int(os.environ.get("ROFI_RETV", "0"))
+    sel = get_selected_id(entries)
+
+    # print(entries)
+    # print("SEL" + str(sel))
+
+
+    if ret in (10, 11):
+        # Extract the sink-input index from the selected entry
+
+        idx = str(entries[sel]["id"])
+
+        # Decie whether to raise or lower volume
+        change = f"{'-' if ret == 10 else '+'}{STEP}"
+        subprocess.run(
+            ["pactl", "set-sink-input-volume", idx, change],
+            check=True
+        )
+
+        # Reprint updated menu
+        entries = build_menu()
+        print_menu(entries)
+
+    else:
+        print_menu(entries)
+
+if __name__ == "__main__":
+    sys.exit(main())
+
