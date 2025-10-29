97e6ca102dce1e16a85192b11965b186fc81c72a
Nuno Ramos <nuno.m.ramos@tecnico.ulisboa.pt>
2025-10-29 17:51:20 +0000

    fix to wezterm for windows

--- stow/config/wezterm/wezterm.lua
+++ stow/config/wezterm/wezterm.lua
@@ -7,8 +7,17 @@ local config = wezterm.config_builder()
 
 -- config.enable_wayland = false
 
+config.wsl_domains = {
+	{
+		name = "WSL:Ubuntu-22.04",
+		distribution = "Ubuntu-22.04",
+		default_cwd = "~",
+		default_prog = { "fish" },
+	},
+}
+
 config.default_prog = { "tmux" }
--- config.default_domain = "WSL:Ubuntu-22.04"
+config.default_domain = "WSL:Ubuntu-22.04"
 -- config.debug_key_events = true
 config.warn_about_missing_glyphs = false
 -- This will hold the configuration.
@@ -24,24 +33,30 @@ config.font = wezterm.font_with_fallback({
 config.font_size = 16
 config.underline_thickness = 4
 
-local background = require("background")
-config.background = "#131A21"
+config.background = {
+	{
+		source = {
+			Color = "#131a21",
+		},
+		width = "110%",
+		horizontal_offset = "-5%",
+		height = "110%",
+		vertical_offset = "-5%",
+		opacity = 1,
+	},
+}
 
 config.color_scheme = "BlueDolphin"
 config.colors = require("./theme")
 
-local tabbar = require("tabBar")
-tabbar.apply_to_config(config)
 config.show_new_tab_button_in_tab_bar = false
-local keys = require("keybinds")
-keys.apply_to_config(config)
 
 config.max_fps = 165
 -- config.front_end = "WebGpu"
 
 config.cursor_thickness = 2
 
-config.window_decorations = "NONE"
+config.window_decorations = "RESIZE"
 config.window_close_confirmation = "NeverPrompt"
 config.hide_mouse_cursor_when_typing = true
 config.hide_tab_bar_if_only_one_tab = true
@@ -53,5 +68,10 @@ config.window_padding = {
 	top = 5,
 	bottom = 0,
 }
+
+wezterm.on("gui-startup", function(cmd)
+	local _, _, window = mux.spawn_window(cmd or {})
+	window:gui_window():maximize()
+end)
 
 return config
