e474389e0c43677193e11bb59639930cf80cf600
Nuno Ramos <nmiguel123@gmail.com>
2026-06-24 22:21:07 +0100

    removed unnecessary boilerplate from config

--- hosts/tower/configuration.nix
+++ hosts/tower/configuration.nix
@@ -12,7 +12,7 @@
     hyprland.enable = true;
     hyprland.monitorsFile = ./monitors.lua;
     sddm.enable = true;
-    plymouth.enable = true;
+    # plymouth.enable = true;
     audio.enable = true;
     fish.enable = true;
     logitech.enable = true;
@@ -84,11 +84,6 @@
   boot.kernelPackages = pkgs.linuxPackages_latest;
 
   networking.hostName = "tower"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
   # Enable networking
   networking.networkmanager.enable = true;
@@ -98,21 +93,6 @@
 
   # Select internationalisation properties.
   i18n.defaultLocale = "en_US.UTF-8";
-
-  # i18n.extraLocaleSettings = {
-  #     LC_IDENTIFICATION = "en_US.UTF-8";
-  #     LC_MEASUREMENT = "en_US.UTF-8";
-  #     LC_MONETARY = "en_US.UTF-8";
-  #     LC_NAME = "en_US.UTF-8";
-  #     LC_NUMERIC = "en_US.UTF-8";
-  #     LC_PAPER = "en_US.UTF-8";
-  #     LC_TELEPHONE = "en_US.UTF-8";
-  #     LC_TIME = "en_US.UTF-8";
-  # };
-
-  # Enable the GNOME Desktop Environment.
-  # services.xserver.displayManager.gdm.enable = true;
-  # services.xserver.desktopManager.gnome.enable = true;
 
   services.xserver.enable = true;
 
@@ -142,14 +122,10 @@
     ];
   };
 
-  # Install firefox.
   programs.firefox.enable = true;
 
-  # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
 
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
   nix.settings.experimental-features = [
     "nix-command"
     "flakes"
@@ -162,31 +138,6 @@
     git
   ];
 
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "25.11"; # Did you read the comment?
 
 }
