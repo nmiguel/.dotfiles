0b792c0a96d1c70a532db21f913de846c1916e66
Nuno Ramos <nmiguel123@gmail.com>
2026-06-24 22:21:24 +0100

    hardcoded monitor variables

--- hosts/tower/monitors.lua
+++ hosts/tower/monitors.lua
@@ -7,14 +7,14 @@
 local vars = require("config.vars")
 
 hl.monitor({
-	output = vars.monitor1,
+	output = "DP-2",
 	mode = "2560x1440@165",
 	position = "1080x480",
 	scale = "1",
 })
 
 hl.monitor({
-	output = vars.monitor2,
+	output = "DP-3",
 	mode = "1920x1080",
 	position = "0x0",
 	scale = "1",
